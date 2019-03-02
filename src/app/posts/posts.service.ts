d143ba32145a884d87251955a57be937c50b9f20
emkeris <emkeris@gmail.com>
2019-03-02 17:03:42 +0000

    post add section works but doesn't store info anywhere

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -9,12 +9,10 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, post: Post[]}>('http://localhost:3000/api/posts')
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
     .subscribe((postData) => {
       this.posts = postData.posts;
       this.postsUpdated.next([...this.posts]);
@@ -27,7 +25,11 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    .subscribe(responseData => {
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }
